4b76e4f00cb685cd83ee855f52e11276ac0d606f
Leonardo Paixão <leonardohspaixao@gmail.com>
2021-04-07 22:14:57 -0300

    bird class and creation of the birs

--- FlappyBird.py
+++ FlappyBird.py
@@ -17,3 +17,30 @@ BIRD_IMAGES = [
 
 pygame.font.init()
 POINT_FONT = pygame.font.SysFont('Times New Roman', 32)
+
+
+class bird:
+    IMGS = BIRD_IMAGES
+    ROTATION_MAX = 25
+    ROTATION_SPEED = 20
+    ANIMATION_TIME = 5
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.angle = 0
+        self.speed = 0
+        self.widght = self.y
+        self.time = 0
+        self.image_score = 0
+        self.image = IMGS[0]
+
+    
+
+
+class pipe:
+    pass
+
+
+class floor:
+    pass
