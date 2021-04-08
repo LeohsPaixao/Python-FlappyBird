14564f9d47d9b33d0507696bca93783c3d07334e
Leonardo Paixão <leonardohspaixao@gmail.com>
2021-04-07 22:27:47 -0300

    jump and move bird function done

--- FlappyBird.py
+++ FlappyBird.py
@@ -35,7 +35,31 @@ class bird:
         self.image_score = 0
         self.image = IMGS[0]
 
-    
+    def jump(self):
+        self.speed = 10.5
+        self.time = 0
+        self.widght = self.y
+
+    def move_Bird(self):
+        # Calcular o deslocamento
+        self.time += 1
+        displacement = 1.5 * (self.time**2) + self.speed * self.time
+
+        # Restringir o deslocamento
+        if displacement > 16:
+            displacement = 16
+        elif displacement < 0:
+            displacement -= 2
+
+        self.y += displacement
+
+        # O Angulo do Passaro
+        if displacement < 0 or self.y < (self.widght + 50):
+            if self.angle < self.ROTATION_MAX:
+                self.angle = self.ROTATION_MAX
+        else:
+            if self.angle > -90:
+                self.angle = self.ROTATION_SPEED
 
 
 class pipe:
