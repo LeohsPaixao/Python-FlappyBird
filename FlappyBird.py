f8fa7a58c7dc728b9059e4f5d13cd26e0390c727
Leonardo Paixão <leonardohspaixao@gmail.com>
2021-04-08 11:29:22 -0300

    floor class code done

--- FlappyBird.py
+++ FlappyBird.py
@@ -140,4 +140,27 @@ class pipe:
 
 
 class floor:
-    pass
+    SPEED = 5
+    WIDGHT = FLOOR_IMAGE.get_width()
+    IMAGE = FLOOR_IMAGE
+
+    def __init__(self, y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.WIDGHT
+
+    def move(self):
+        self.x1 -= self.SPEED
+        self.x2 -= self.SPEED
+
+        if self.x1 + self.WIDGHT < 0:
+            self.x1 = self.x1 + self.WIDGHT
+        if self.x2 + self.WIDGht < 0:
+            self.x2 = self.x2 + self.WIDGHT
+
+    def draw(self, screen):
+        screen.blit(self.IMAGE, (self.x1, self.y))
+        screen.blit(self.IMAGE, (self.x2, self.y))
+    
+
+    
