bac45ab4e1c73e0490c7b2365659d2d3feb0dc29
Leonardo Paixão <leonardohspaixao@gmail.com>
2021-04-11 00:11:10 -0300

    Implemented to NEAT no program

--- FlappyBird.py
+++ FlappyBird.py
@@ -3,12 +3,12 @@ import os
 import random
 import neat
 
-ai_gaming = True
-generation = 0
 
+ai_jogando = True
+geracao = 0
 
-SCREEN_WIDTH = 500
-SCREEN_HEIGHT = 800
+TELA_LARGURA = 500
+TELA_ALTURA = 800
 
 IMAGEM_CANO = pygame.transform.scale2x(
     pygame.image.load(os.path.join('imgs', 'pipe.png')))
@@ -26,7 +26,7 @@ IMAGENS_PASSARO = [
 ]
 
 pygame.font.init()
-FONTE_PONTOS = pygame.font.SysFont('arial', 50)
+FONTE_PONTOS = pygame.font.SysFont('times new roman', 32)
 
 
 class Passaro:
@@ -76,19 +76,14 @@ class Passaro:
         # definir qual imagem do passaro vai usar
         self.contagem_imagem += 1
 
-        # TEMPO_ANIMACAO(5 seg) = 5 seg
         if self.contagem_imagem < self.TEMPO_ANIMACAO:
             self.imagem = self.IMGS[0]
-        # TEMPO_ANIMACAO(5 seg) * 2 = 10 seg
         elif self.contagem_imagem < self.TEMPO_ANIMACAO*2:
             self.imagem = self.IMGS[1]
-        # TEMPO_ANIMACAO(5 seg) * 3 = 15 seg
         elif self.contagem_imagem < self.TEMPO_ANIMACAO*3:
             self.imagem = self.IMGS[2]
-        # TEMPO_ANIMACAO(5 seg) * 4 = 20 seg
         elif self.contagem_imagem < self.TEMPO_ANIMACAO*4:
             self.imagem = self.IMGS[1]
-        # TEMPO_ANIMACAO(5 seg) * 4 + 1 = 21 seg
         elif self.contagem_imagem >= self.TEMPO_ANIMACAO*4 + 1:
             self.imagem = self.IMGS[0]
             self.contagem_imagem = 0
@@ -184,21 +179,35 @@ def desenhar_tela(tela, passaros, canos, chao, pontos):
         cano.desenhar(tela)
 
     texto = FONTE_PONTOS.render(f"Pontuação: {pontos}", 1, (255, 255, 255))
-    tela.blit(texto, (SCREEN_WIDTH - 10 - texto.get_width(), 10))
+    tela.blit(texto, (TELA_LARGURA - 10 - texto.get_width(), 10))
 
-    if ai_gaming:
-        texto = FONTE_PONTOS.render(f"Geração: {generation}", 1, (255, 255, 255))
+    if ai_jogando:
+        texto = FONTE_PONTOS.render(f"Geração: {geracao}", 1, (255, 255, 255))
         tela.blit(texto, (10, 10))
 
     chao.desenhar(tela)
     pygame.display.update()
 
 
-def main():
-    passaros = [Passaro(230, 350)]
+def main(genomas, config):  # fitness function
+    global geracao
+    geracao += 1
+
+    if ai_jogando:
+        redes = []
+        lista_genomas = []
+        passaros = []
+        for _, genoma in genomas:
+            rede = neat.nn.FeedForwardNetwork.create(genoma, config)
+            redes.append(rede)
+            genoma.fitness = 0
+            lista_genomas.append(genoma)
+            passaros.append(Passaro(230, 350))
+    else:
+        passaros = [Passaro(230, 350)]
     chao = Chao(730)
     canos = [Cano(700)]
-    tela = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    tela = pygame.display.set_mode((TELA_LARGURA, TELA_ALTURA))
     pontos = 0
     relogio = pygame.time.Clock()
 
@@ -212,14 +221,31 @@ def main():
                 rodando = False
                 pygame.quit()
                 quit()
-            if evento.type == pygame.KEYDOWN:
-                if evento.key == pygame.K_SPACE:
-                    for passaro in passaros:
-                        passaro.pular()
+            if not ai_jogando:
+                if evento.type == pygame.KEYDOWN:
+                    if evento.key == pygame.K_SPACE:
+                        for passaro in passaros:
+                            passaro.pular()
+
+        indice_cano = 0
+        if len(passaros) > 0:
+            if len(canos) > 1 and passaros[0].x > (canos[0].x + canos[0].CANO_TOPO.get_width()):
+                indice_cano = 1
+        else:
+            rodando = False
+            break
 
         # mover as coisas
-        for passaro in passaros:
+        for i, passaro in enumerate(passaros):
             passaro.mover()
+            # aumentar um pouquinho a fitness do passaro
+            lista_genomas[i].fitness += 0.1
+            output = redes[i].activate((passaro.y,
+                                        abs(passaro.y - canos[indice_cano].altura),
+                                        abs(passaro.y - canos[indice_cano].pos_base)))
+            # -1 e 1 -> se o output for > 0.5 então o passaro pula
+            if output[0] > 0.5:
+                passaro.pular()
         chao.mover()
 
         adicionar_cano = False
@@ -228,6 +254,10 @@ def main():
             for i, passaro in enumerate(passaros):
                 if cano.colidir(passaro):
                     passaros.pop(i)
+                    if ai_jogando:
+                        lista_genomas[i].fitness -= 1
+                        lista_genomas.pop(i)
+                        redes.pop(i)
                 if not cano.passou and passaro.x > cano.x:
                     cano.passou = True
                     adicionar_cano = True
@@ -238,15 +268,40 @@ def main():
         if adicionar_cano:
             pontos += 1
             canos.append(Cano(600))
+            for genoma in lista_genomas:
+                genoma.fitness += 5
         for cano in remover_canos:
             canos.remove(cano)
 
         for i, passaro in enumerate(passaros):
             if (passaro.y + passaro.imagem.get_height()) > chao.y or passaro.y < 0:
                 passaros.pop(i)
+                if ai_jogando:
+                    lista_genomas.pop(i)
+                    redes.pop(i)
 
         desenhar_tela(tela, passaros, canos, chao, pontos)
 
 
+def rodar(caminho_config):
+    config = neat.config.Config(neat.DefaultGenome,
+                                neat.DefaultReproduction,
+                                neat.DefaultSpeciesSet,
+                                neat.DefaultStagnation,
+                                caminho_config
+                                )
+
+    populacao = neat.Population(config)
+    populacao.add_reporter(neat.StdOutReporter(True))
+    populacao.add_reporter(neat.StatisticsReporter())
+
+    if ai_jogando:
+        populacao.run(main, 50)  # Roda em 50 gerações
+    else:
+        main(None, None)
+
+
 if __name__ == '__main__':
-    main()
+    caminho = os.path.dirname(__file__)
+    caminho_config = os.path.join(caminho, 'config.txt')
+    rodar(caminho_config)
